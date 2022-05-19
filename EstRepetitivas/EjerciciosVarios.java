dc00c394ad8008a8c0dd1dfd6ba4a3a41e9c6f25
eberthrosales <eberthrosalescastillo@gmail.com>
2022-05-19 17:58:18 +0000

    repetitivas

--- EstRepetitivas/EjerciciosVarios.java
+++ EstRepetitivas/EjerciciosVarios.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class EjerciciosVarios {
     static Scanner leerT=new Scanner(System.in);
 
-    public static int factorialN(int numero) {
-        int resultado=1;
+    public static long factorialN(int numero) {
+        long resultado=1;
         if (numero>1) {
             for (int i = 1; i <= numero; i++) {
                 resultado=resultado*i;
@@ -17,8 +17,12 @@ public class EjerciciosVarios {
             } 
         return resultado;
     }
-
     public static void main(String[] args) {
-        System.out.println(factorialN(5));
+        System.out.println("Ingrese un numero");
+        int num=leerT.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println("Factorial de "+ i+"es:"+factorialN(i));
+            
+        }
     }
 }
