6f12ff328a4266368131af3bc3d91472f519f5a0
eberthrosales <eberthrosalescastillo@gmail.com>
2022-05-26 17:32:58 +0000

    letra

---
+++ subprogramas/Graficarletras.java
@@ -0,0 +1,38 @@
+package subprogramas;
+
+import java.util.Scanner;
+
+public class Graficarletras {
+
+    static Scanner leerT=new Scanner(System.in);
+
+    public static void graficarA() {
+        System.out.println("Ingrese Altura de la Letra:");
+        int cantF=leerT.nextInt();
+        System.out.println("Ingrese num fila para Linea:");
+        int numFilLinea=leerT.nextInt();
+        int medio=cantF+1;
+        for (int f = 1; f <= cantF; f++) {
+            for (int c = 1; c <= 2*cantF; c++) {
+                if (f==1 && medio==c ) {
+                    System.out.print("*");
+                }else if(c==medio-f && f>1){
+                    System.out.print("*");
+                }else if(c==medio+f && f>1){
+                    System.out.print("*");
+                }else if(f==numFilLinea && c>medio-f && c<=medio+f){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");                    
+                }                
+            }
+            System.out.println("\n");
+        }
+        System.out.println("");
+
+    }
+    public static void main(String[] args) {
+        graficarA();
+
+    }
+}
