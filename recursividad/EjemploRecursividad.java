5d4a968cc381d3735d7428b77caa32a4983f7159
eberthrosales <eberthrosalescastillo@gmail.com>
2022-05-31 15:40:46 +0000

    recursividad

---
+++ recursividad/EjemploRecursividad.java
@@ -0,0 +1,42 @@
+package recursividad;
+
+public class EjemploRecursividad {
+
+    public static int factorialR(int n) {
+        if (n>1) {
+            return factorialR(n-1)*n;
+            
+        }
+        return 1;
+        
+    }
+    public static int fibonaciR(int n) {
+        if (n>1) {
+            return fibonaciR(n-1)+fibonaciR(n-2);
+            
+        }
+        return n;
+        
+    }
+    public static double potencia(int base, int exponente) {
+        if (exponente ==0) {
+            return 1;
+        }else if(exponente ==1){
+            return base;
+        }else if (exponente < 0){
+            return potencia(base, exponente + 1) / base;
+        }else{
+            return base * potencia(base, exponente -1);
+            
+        }
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(factorialR(10));
+        System.out.println("Fibonaci:");
+        System.out.println(fibonaciR(6));
+    }
+}
